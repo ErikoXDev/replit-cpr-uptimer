84bb4e5569880771d95bcb2a26c84f8e34867933
Erik <54033728+ErikoX@users.noreply.github.com>
2022-10-25 14:33:13 +0000

    Update

--- api.js
+++ api.js
@@ -2,13 +2,13 @@
 const db = require("./db")
 
 
-module.exports = function(app){
+module.exports = function(app) {
 
-  app.post('/api/repl', function(req, res){
+  app.post('/api/repl', function(req, res) {
     console.log(req.body)
     let action = req.body.action
     let url = req.body.url
-    if (!url.startsWith("https://")) {url+="https://"}
+    if (!url.startsWith("https://")) { url += "https://" }
     if (!db.stringIsAValidUrl(url)) {
       res.status(400).json({ success: false, message: "Malformed URL" })
       return
@@ -19,15 +19,23 @@ module.exports = function(app){
         res.status(400).json({ success: false, message: "Malformed URL" })
         return
       } else {
-        if (!db.dbdata.ping[owner]) db.dbdata.ping[owner] = {}
-        if (db.dbdata.ping[owner][repl]) {
-          res.status(400).json({ success: false, message:`Repl is already being pinged! <a href='/@${owner}/${repl}' target='_blank' class='underline text-gray-500'>Look at analytics</a>` })
-          return
-        } else {
-          db.dbdata.ping[owner][repl] = {pings:0,lastPing:0,lastUpdate:0,stats:[0,0,0,0,0,0]}
-          res.status(200).json({ success: true, message:"Repl is now getting pinged!" })
-          return
-        }
+        db.ifUserExists(owner).then(exists=>{
+          if (exists) {
+            if (!db.dbdata.ping[owner]) db.dbdata.ping[owner] = {}
+            if (db.dbdata.ping[owner][repl]) {
+              res.status(400).json({ success: false, message: `Repl is already being pinged! <a href='/@${owner}/${repl}' target='_blank' class='underline text-gray-500'>Look at analytics</a>` })
+              return
+            } else {
+              db.dbdata.ping[owner][repl] = { pings: 0, lastPing: 0, lastUpdate: 0, stats: [0, 0, 0, 0, 0, 0] }
+              res.status(200).json({ success: true, message: "Repl is now getting pinged! <a href='/@${owner}/${repl}' target='_blank' class='underline text-gray-500'>Look at analytics</a>" })
+              return
+            }
+          } else {
+            res.status(400).json({ success: false, message: "User doesn't exist! " + owner })
+            return
+          }
+        })
+        
       }
     } else {
       res.status(400).json({ success: false, message: "Invalid Action" })
--- db.js
+++ db.js
@@ -13,20 +13,20 @@ var alive = 100
 
 function percent(part, total) {
   return (100 * part) / total;
-} 
+}
 
-function abbreviate(num,decimals){
-  let numLen=(String(num)).length
-  decimals=Math.pow(10,decimals)
-  numLen-=numLen%3
-  return Math.round(num*decimals/Math.pow(10,numLen))/decimals+" kMGTPE"[numLen/3]
+function abbreviate(num, decimals) {
+  let numLen = (String(num)).length
+  decimals = Math.pow(10, decimals)
+  numLen -= numLen % 3
+  return Math.round(num * decimals / Math.pow(10, numLen)) / decimals + " kMGTPE"[numLen / 3]
 }
 
 
 function getReplData(url) {
-  let clean = filter.clean(url.replace("https://","").split("/")[0].replace(".repl.co",""))
+  let clean = filter.clean(url.replace("https://", "").split("/")[0].replace(".repl.co", ""))
   if (clean.includes("*")) {
-    return ["","URL includes profanity or blacklisted words"]
+    return ["", "URL includes profanity or blacklisted words"]
   }
   let split = clean.split(".")
   if (split.length == 2) {
@@ -36,10 +36,35 @@ function getReplData(url) {
       // Repl exists
       return [repl.toLowerCase(), owner.toLowerCase()]
     } else {
-      return ["","Malformed URL"]
+      return ["", "Malformed URL"]
     }
   } else {
-    return ["","Malformed URL"]
+    return ["", "Malformed URL"]
+  }
+}
+
+async function ifUserExists(user) {
+  let resp = axios.post("https://replit.com/graphql", {
+    query: `query UserByUsername($username: String!) {
+	userByUsername(username: $username) {
+		id
+		username
+	}
+}`, variables: { "username": String(user) }
+  }, {
+    headers: {
+      'User-Agent': 'Mozilla/5.0',
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
+      "Referrer": 'https://replit.com/',
+    }
+  })
+
+  let json = await resp.then(res => { return res })
+  if (json.data.data["userByUsername"] != null) {
+    return true
+  } else {
+    return false
   }
 }
 
@@ -48,13 +73,13 @@ const stringIsAValidUrl = (s) => {
     new URL(s);
     return true;
   } catch (err) {
-    
+
     return false;
   }
 };
 
 function getUnix() {
-  return Math.floor(Date.now()/1000)
+  return Math.floor(Date.now() / 1000)
 }
 
 function readDB() {
@@ -69,25 +94,25 @@ function writeDB(json) {
 let accept = [200, 304, 100, 201, 202, 206, 302]
 
 async function ping(url, urldata) {
-  if (getUnix()-300 > urldata.lastPing) {
+  if (getUnix() - 300 > urldata.lastPing) {
     axios({
       method: "GET",
       url: url,
       timeout: 5000
     }).then((res) => {
-      
+
       dbdata.total++
       urldata.lastPing = getUnix()
-      
+
       let [repl, owner] = getReplData(url)
-      if (repl == "") {return}
+      if (repl == "") { return }
 
       let status = res.status
-      if (!(accept.includes(status))) {status = 500} else {
+      if (!(accept.includes(status))) { status = 500 } else {
         alive++
       }
 
-      if (getUnix()-7200 > urldata.lastUpdate) {
+      if (getUnix() - 7200 > urldata.lastUpdate) {
         urldata.stats.shift()
         urldata.stats.push(status)
         urldata.lastUpdate = getUnix()
@@ -97,8 +122,8 @@ async function ping(url, urldata) {
       urldata.pings++
 
       return urldata
-      
-    }).catch(e=>{"Well, something exceeded the timeout huh!"})
+
+    }).catch(e => { "Well, something exceeded the timeout huh!" })
   } else {
     return {}
   }
@@ -112,8 +137,8 @@ function pingAll() {
     let user = plsPing[key]
     for (var key2 in user) {
       replsPinged++
-      ping("https://"+key2+"."+key+".repl.co", plsPing[key][key2]).then((b) => {
-        
+      ping("https://" + key2 + "." + key + ".repl.co", plsPing[key][key2]).then((b) => {
+
       })
     }
   }
@@ -127,4 +152,4 @@ setInterval(() => {
 }, 5000)
 
 
-module.exports = { getReplData, dbdata, pingAll, abbreviate, replsPinged, alive, percent, stringIsAValidUrl }
+module.exports = { getReplData, dbdata, pingAll, abbreviate, replsPinged, alive, percent, stringIsAValidUrl, ifUserExists }
